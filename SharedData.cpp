a0ba6a7fdb2d27b2ccf80a6cead0923209fa70a4
mikeyq6 <michael.g.dann@gmail.com>
2026-02-10 21:34:04 +0100

    add running variable to SharedData and implement concurrent access

--- SharedData.cpp
+++ SharedData.cpp
@@ -1,1 +1,19 @@
 #include "inc/SharedData.h"
+
+SharedData::SharedData() {
+    is_running = true;
+}
+SharedData::~SharedData() {
+
+}
+
+bool SharedData::get_is_running() {
+    const lock_guard<mutex> lock{is_running_mutex};
+
+    return is_running;
+}
+void SharedData::set_is_running(bool value) {
+    const lock_guard<mutex> lock{is_running_mutex};
+
+    is_running = value;
+}
--- inc/SharedData.h
+++ inc/SharedData.h
@@ -1,1 +1,19 @@
 #pragma once
+
+#include <mutex>
+
+using std::mutex;
+using std::lock_guard;
+
+class SharedData {
+public:
+    SharedData();
+    ~SharedData();
+
+    bool is_running;
+    bool get_is_running();
+    void set_is_running(bool value);
+
+private:
+	mutex is_running_mutex;
+};
